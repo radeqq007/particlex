397dbcc012e3c691e154022658246415c728a957
radeqq007 <radoslaw.kaczmarczyk@proton.me>
2026-05-03 20:31:08 +0200

    feat: add base functionality

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+export interface Particle {
+  x: number;
+  y: number;
+  life?: number;
+  [key: string]: any;
+}
+
+export interface Config {
+  count: number;
+  initialLife?: number;
+  keepDead?: boolean;
+}
+
+export interface Options {
+  init: (index: number) => Particle;
+  update: (p: Particle, dt: number) => void;
+  render: (p: Particle, ctx: CanvasRenderingContext2D, dt: number) => void;
+}
+
+export const createParticles = (canvas: string, { init, update, render }: Options, config: Config) => {
+  const c = document.querySelector(canvas) as HTMLCanvasElement;
+  const ctx = c.getContext('2d');
+  if (!ctx) return;
+
+  config = { initialLife: 1, keepDead: false, ...config };
+
+  let particles: Particle[] =Array.from(
+    { length: config.count },
+    (_, i) => {
+      const p = init(i);
+      return { ...p, life: p.life ?? config.initialLife }
+    });
+  
+  let lastTime = 0;
+
+  const loop = (currentTime: number) => {
+    const dt = (currentTime - lastTime) / 1000;
+
+    lastTime = currentTime;
+    ctx.clearRect(0, 0, c.width, c.height);
+    
+    for (const p of particles) {
+      update(p, dt);
+    }
+
+    if (!config.keepDead) {
+      particles = particles.filter(p => p.life! > 0);
+    }
+
+    for (const p of particles) {
+      render(p, ctx, dt);
+    }
+
+    requestAnimationFrame(loop);
+  }
+
+  requestAnimationFrame(loop);
+}
+
